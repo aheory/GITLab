d14d60d4220791bf833d07119855a14845d9e33c
aheory <dawqneh@vk.com>
2024-10-10 20:26:38 +0300

    operation div added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿case ConsoleKey.D6:
+case ConsoleKey.NumPad6:
+    if (b == 0)
+    {
+        Console.WriteLine("Ошибка деление на ноль");
+        break;
+    }
+    Console.WriteLine($"\nРезультат операции «/»: {a} * {b} = {a * b}");
+    Console.ReadKey();
+    break;
